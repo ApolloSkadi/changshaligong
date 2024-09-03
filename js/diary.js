4e368e3d5e3ce721f13985af29ad2c313b24c17b
apollo <763753335@qq.com>
2024-09-03 23:39:49 +0800

    日记 新增多张图片 汇报模板补充

--- js/diary.js
+++ js/diary.js
@@ -3,6 +3,11 @@ const diaryData = [
         title: '日记标题', // 日记标题
         date: '2024-08-08', // 日记日期
         author: 'apollo', // 日记作者
+        imgs:[ // 详情页面展示的图片
+            '../images/home/cslgdx.png',
+            '../images/home/cslgdx.png',
+            '../images/home/cslgdx.png',
+        ],
         img: '../images/home/cslgdx.png', // 日记缩略图
         text: '今天是八月八日，星期日。', // 日记正文
     },
